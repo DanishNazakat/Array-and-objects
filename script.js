b5566a9409b44077424c637d260d87dd9b933f0f
DanishNazakat <danishnazakat636@gmail.com>
2025-07-08 15:59:22 -0700

    Array and Object Task

--- script.js
+++ script.js
@@ -48,8 +48,61 @@ let dewanGroup = [
                     company4: "Dewan Salman Fibre Ltd"
                 }
 
+            },
+            {
+                name: "Dewan Ethanol Manufactureing",
+                uses: ["Alcohol Industry", "Pharmaceutical Industry", "HouseHold Chemicals ", "WindScreen Washing Fluid", "Food Industry", "Cosmetic and perfume Industry", "Industrial Chemicals"],
+                products: [
+                    {
+                        name: "ANH Anhydrous Alcohol",
+                        purity:"99.9 % Min"
+                    },
+                    {
+                        name: "ENA  Neutral Alcohol",
+                        purity:"96.2 % Min"
+                    },
+                    {
+                        name: "B - Grade Alcohol",
+                        purity:"93 % Min"
+                    },
+                ],
+                supplyOptions: ["Bulk Vessel","ISO tank","FCL"]
+
             }
-        ]
+        ],
+        University: {
+            name: " Shaheed Benazir Bhutto Dewan University (SBBDU)",
+            location: "30 A, Mehran Town Sector 23 Korangi Industrial Area, Karachi Hours ",
+            Faculties: [
+                {
+                    Name: " Doctor of Physical Therapy - DPT",
+                    duration: "5 year",
+                    contact: "03368729555",
+                    Addmission: "Fall 2025 admissions are open. ",
+                    Requirments: "An FSc (Pre-Medical) degree with at least 60% marks is typically required for admission. "
+                },
+                {
+                    Name: "Pharm D (Doctor of Pharmacy)",
+                    duration: "5 year",
+                    contact: "03368729555",
+                    Addmission: "Fall 2025 admissions are open. ",
+                    Requirments: "An FSc (Pre-Medical) degree with at least 60% marks is typically required for admission. "
+
+                },
+                {
+                    name: " Management Sciences (BBA)",
+                    duration: "4 year",
+                    Addmission: "Close",
+                    Requirments: "Minimum 45% marks in Intermediate or equivalent"
+                },
+                {
+                    name: " Information Technology (BSCS)",
+                    duration: "4 year",
+                    Addmission: "Close",
+                    Requirments: "minimum of 50% marks in your Intermediate (HSSC) or equivalent qualification, with a preference for candidates having a mathematical background"
+                }
+            ]
+        }
     }
 ]
 
